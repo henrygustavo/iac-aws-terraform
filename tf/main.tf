f23c75c70dc6936c896dd1a609a5b459545d28ea
henry fuentes <henrygustavof@gmail.com>
2021-10-21 21:35:23 -0500

    sns sqs

--- tf/main.tf
+++ tf/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region                      = "us-west-1"
   access_key                  = "anaccesskey"
-  secret_key                  = "asecretkey"
+  secret_key                  = "anaccesskey"
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   s3_force_path_style         = true
@@ -13,6 +13,16 @@ provider "aws" {
     sqs            = "http://aws-localstack:4566"
     dynamodb       = "http://aws-localstack:4566"
   }
+}
+
+variable "environment" {
+  default = "local"
+}
+
+
+locals {
+  environment = "${var.environment}"
+  domain      = "system"
 }
 
 resource "aws_dynamodb_table" "system-db" {
@@ -31,4 +41,22 @@ resource "aws_dynamodb_table" "system-db" {
     name = "Version"
     type = "N"
   }
+}
+
+resource "aws_sns_topic" "system-sns-topic" {
+  name = "${local.environment}-${local.domain}-sns-topic"
+}
+
+
+# SQS Standard Queues
+resource "aws_sqs_queue" "system-sqs-queue" {
+  name                       = "${local.environment}-${local.domain}-sqs-queue"
+  visibility_timeout_seconds = 45
+}
+
+resource "aws_sns_topic_subscription" "system--subscription" {
+  topic_arn            = "${aws_sns_topic.system-sns-topic.arn}"
+  protocol             = "sqs"
+  endpoint             = "${aws_sqs_queue.system-sqs-queue.arn}"
+  raw_message_delivery = true
 }
